f0620852e6c8c4bd2a728cc2bb6f1a0a1f3a149b
Shilpa Patwary <shilpa.patwary@sapient.com>
2019-10-14 09:58:45 +0530

    modifying contact now

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
      });
      $('#contact_form').on('submit', function(e) {
         e.preventDefault();
+        $('.loading').show();
         const form = $('#contact_form');
         const url = "https://script.google.com/macros/s/AKfycbybU6KFMDUArUMdo87OcHtM97okKqcHELE-D-V5Tw/exec";
         $.ajax({
@@ -24,6 +25,11 @@ $(document).ready(function(){
             data: form.serialize(),
             success: function(data) {
                 $(".success").show().delay(4000).hide();
+                $('.loading').hide();
+                $('.success').show();
+                setTimeout(function() {
+                    $('.success').fadeOut('fast');
+                }, 3000); 
                 form.trigger("reset");
             }
         })
